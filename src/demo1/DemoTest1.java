838ce8d1cf027d24311db4fc8dfe36cbdc36975f
Uncle Ricco <eric4cali@gmail.com>
2025-01-29 18:29:56 -0800

    Update DemoTest1.java
    Test

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -7,5 +7,16 @@ public class DemoTest1 {
 	
 	int wealth = 346000000;
 	System.out.println(wealth);
+
+	int a = 10;
+		int b = 20;
+		
+		if (a > b) {
+			System.out.println("Greater Than");
+		} else {
+			System.out.println("Less Than");
+			
+		}
+		
 }
 }
